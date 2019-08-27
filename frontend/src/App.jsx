2f916d4ff6aa1997343458a33f92ee89366c30ff
yarbiv <yoav.arbiv@gmail.com>
2019-08-27 16:54:15 -0400

    Barebones input field

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import ClipLoader from 'react-spinners/ClipLoader';
+import { css } from '@emotion/core';
+import ImageContainer from './ImageContainer';
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 function App() {
   const [apiData, setApiData] = useState(null);
+  const [input, setInput] = useState('');
 
-  function getDataRoutes(artistName) {
+  function getDataRoutes() {
+    setApiData('fetching');
     axios
       .get('http://localhost:5000/analyze', {
         params: {
-          artist_name: artistName,
+          artist_name: input,
         },
       })
       .then((response) => {
         console.log(response);
-        setApiData(response);
+        setApiData(response.data);
       });
   }
 
-  useEffect(getDataRoutes);
-
   return (
     <div className="App">
-      <header className="App-header" />
-      <p>{apiData ? apiData.length : 'Loading'}</p>
+      <header className="App-header">
+        <form>
+          <input value={input} onInput={(e) => setInput(e.target.value)} />
+          <button type="button" onClick={getDataRoutes}>Go</button>
+          <ImageContainer apiData={apiData} />
+          <ClipLoader
+            css={override}
+            loading={apiData === 'fetching'}
+          />
+        </form>
+      </header>
     </div>
   );
 }
---
+++ frontend/src/ImageContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ImageContainer = (props) => (
+  // Needs to get through all apiData paths and show the needed saved files
+  // Obviously a wip
+  // <div>
+  //   <img src={apiData && apiData !== 'fetching' ? `http://localhost:5000/${apiData[0]}` : null} />
+  //
+  // </div>
+  <div />
+);
+
+export default ImageContainer;
